5f6d6afb845791bb45cd865692284de4522b1560
EvilPug <3608690@mail.ru>
2018-10-17 16:10:42 +0300

    Реализована функция encrypt_vigenere()

--- vigenere.py
+++ vigenere.py
@@ -9,7 +9,34 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    alphabet_low = list('abcdefghigklmnopqrstuvwxyz')
+    alphabet_high = list('ABCDEFGHIGKLMNOPQRSTUVWXYZ')
+    plainlist = list(plaintext)
+    keylist = list(keyword)
+    ciphertext = ''
+
+    if plaintext != '':
+        n = 0
+        if len(plaintext) != len(keyword):
+            for i in range(len(plaintext) - len(keyword)):
+                keylist += keylist[n]
+                n = n + 1
+        first = (ord(plainlist[0]))
+        if ((first >= 97) and (first <= 122)):
+            n = 0
+            for abc in plaintext:
+                newnumb = 97 + (ord(abc) + alphabet_low.index(keylist[n]) - 97) % 26
+                ciphertext += chr(newnumb)
+                n = n + 1
+        elif ((first >= 65) and (first <= 90)):
+            n = 0
+            for abc in plaintext:
+                newnumb = 65 + (ord(abc) + alphabet_high.index(keylist[n]) - 65) % 26
+                ciphertext += chr(newnumb)
+                n = n + 1
+        else:
+            for abc in plaintext:
+                newnumb = ord(abc)
     return ciphertext
 
 

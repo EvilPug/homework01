8771e97f8b3de8dee073e44fc73fa04f916c3916
EvilPug <3608690@mail.ru>
2018-10-17 16:11:40 +0300

    Реализована функция decrypt_vigenere()

--- vigenere.py
+++ vigenere.py
@@ -51,5 +51,33 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    alphabet_low = list('abcdefghigklmnopqrstuvwxyz')
+    alphabet_high = list('ABCDEFGHIGKLMNOPQRSTUVWXYZ')
+    cipherlist = list(ciphertext)
+    keylist = list(keyword)
+    plaintext = ''
+
+    if ciphertext != '':
+        n = 0
+        if len(ciphertext) != len(keyword):
+            for i in range(len(ciphertext) - len(keyword)):
+                keylist += keylist[n]
+                n = n + 1
+        first = (ord(cipherlist[0]))
+        if ((first >= 97) and (first <= 122)):
+            n = 0
+            for abc in ciphertext:
+                newnumb = 97 + (ord(abc) - alphabet_low.index(keylist[n]) - 97) % 26
+                plaintext += chr(newnumb)
+                n = n + 1
+        elif ((first >= 65) and (first <= 90)):
+            n = 0
+            for abc in ciphertext:
+                newnumb = 65 + (ord(abc) - alphabet_high.index(keylist[n]) - 65) % 26
+                plaintext += chr(newnumb)
+                n = n + 1
+        else:
+            for abc in plaintext:
+                newnumb = ord(abc)
+
     return plaintext

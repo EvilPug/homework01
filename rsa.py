9af408bad3b34a5f8b4aa46b84a2747ce57aa73e
EvilPug <3608690@mail.ru>
2018-10-25 20:53:42 +0300

    Внесены правки (x3)

--- rsa.py
+++ rsa.py
@@ -15,7 +15,6 @@ def is_prime(n):
         for i in range(2, n):
             if (n % i) == 0:
                 return False
-                break
         else:
             return True
     else:
